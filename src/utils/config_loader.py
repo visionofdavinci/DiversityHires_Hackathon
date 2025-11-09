464d47b9d74adef198e9520483f9c86ee1c5884f
visionofdavinci <145295500+visionofdavinci@users.noreply.github.com>
2025-11-09 14:44:31 +0100

    Fix NoneType error in config_loader - Handle missing env variables

--- src/utils/config_loader.py
+++ src/utils/config_loader.py
@@ -6,12 +6,16 @@ import os
 load_dotenv()
 
 def get_config():
+    # Get scopes with default value to prevent None.split() errors
+    scopes = os.getenv("GOOGLE_CALENDAR_SCOPES", "https://www.googleapis.com/auth/calendar.readonly")
+    usernames = os.getenv("LETTERBOXD_USERNAMES", "")
+    
     return {
         "GOOGLE_CREDENTIALS_PATH": os.getenv("GOOGLE_CREDENTIALS_PATH"),
         "GOOGLE_TOKEN_PATH": os.getenv("GOOGLE_TOKEN_PATH"),
-        "GOOGLE_CALENDAR_SCOPES": os.getenv("GOOGLE_CALENDAR_SCOPES").split(","),
+        "GOOGLE_CALENDAR_SCOPES": scopes.split(",") if scopes else ["https://www.googleapis.com/auth/calendar.readonly"],
         "TMDB_API_KEY": os.getenv("TMDB_API_KEY"),
-        "LETTERBOXD_USERNAMES": os.getenv("LETTERBOXD_USERNAMES").split(","),
+        "LETTERBOXD_USERNAMES": usernames.split(",") if usernames else [],
         "GOOGLE_TOKENS_FOLDER": os.getenv("GOOGLE_TOKENS_FOLDER"),
         "SHOWTIME_DEBUG": os.getenv("SHOWTIME_DEBUG", "0") == "1",
         "USE_CALENDAR": os.getenv("USE_CALENDAR", "0") == "1",
